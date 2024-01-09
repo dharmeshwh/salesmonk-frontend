b378bdb23c4159fe8c78b67e50404da5af0cf312
dharmeshwh <dharmeshaheer@gmail.com>
2024-01-09 23:35:29 +0530

    added public ip

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const baseApi = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "http://18.221.58.137/api/v1",
 });
 
 export const API_URLS = {
